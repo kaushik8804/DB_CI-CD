7e19b241d4bc9d8b6e9c298babc352fb2a0ad63c
KAUSHIK RANJAN SINHA <sinhakaushik98@gmail.com>
2025-01-19 14:27:14 +0530

    Add test scripts for GitHub Actions

--- sql-scripts/create_table.sql
+++ sql-scripts/create_table.sql
@@ -1,6 +1,4 @@
-CREATE TABLE temp_employees (
+CREATE TABLE test_table (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    role VARCHAR(50),
-    salary NUMERIC(10, 2)
+    message VARCHAR(100) NOT NULL
 );
